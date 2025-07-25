213e61bf68cf70e86731d5dbfc19740bf52109f0
Sivabala V <23d247@psgtech.ac.in>
2025-07-25 18:55:01 +0530

    [1] [Time Beats: 13.37%] [Memory Beats: 59.72%] - LeetPush

---
+++ Two-Sum.c
@@ -0,0 +1,17 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    int* res=(int*)malloc(sizeof(int)*2);
+    *returnSize=2;
+    for(int i=0;i<numsSize;i++){
+    
+        for(int j=i+1;j<numsSize;j++){
+            if(nums[j]+nums[i]==target){
+                res[0]=i;
+                res[1]=j;
+            }
+        }
+    }
+    return res;
+}
