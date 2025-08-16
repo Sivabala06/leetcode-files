da07838d56455def60f7ae61506b33b094d212d4
Sivabala V <23d247@psgtech.ac.in>
2025-08-16 09:51:11 +0530

    [17] [Time Beats: 100.00%] [Memory Beats: 39.74%] - LeetPush

--- Letter-Combinations-of-a-Phone-Number.cpp
+++ Letter-Combinations-of-a-Phone-Number.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
- void traverse(string digits ,string mapping[],string curr,vector<string>&res){
+ /*void traverse(string digits ,string mapping[],string curr,vector<string>&res){
     if(digits.empty()) res.push_back(curr);
     else{
         string letter=mapping[digits[0]-'0'];
@@ -13,6 +13,21 @@ public:
         if(digits.empty()) return {};
         string mapping[]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
         traverse(digits,mapping,"",res);
+        return res;*/
+    vector<string> letterCombinations(string digits) {
+        if(digits.empty()) return {};
+        string mapping[]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        vector<string>res={""};
+        for(char digit :digits){
+            vector<string>curr;
+            for(string comb:res){
+                for(char choice:mapping[digit-'0']){
+                    curr.push_back(comb+choice);
+                }
+            }
+            res=curr;
+        }
         return res;
-    }
+        
+     }
 };
