ded4ac30d33efe21348002f4bb51d4740de34836
Sivabala V <23d247@psgtech.ac.in>
2026-07-26 17:30:17 +0530

    [35] [Time Beats: 4.49%] [Memory Beats: 42.87%] - LeetPush

--- Sivabala06/leetcode-files/35-Search-Insert-Position.py
+++ Sivabala06/leetcode-files/35-Search-Insert-Position.py
@@ -1,15 +1,7 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        n=len(nums)-1
-        for i in range(n):
-            if nums[i]==target :
-                return i
-            elif nums[i] < target and nums[i+1]>=target:
-                return i+1
-        if nums[n] < target :
-            return len(nums)
-        else:
-            return 0
+       for i in range(len(nums)):
+        if nums[i] >= target:
+            return i
 
-          
-        
+       return len(nums)
