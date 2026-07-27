6e98e188399d622661699b3e07cc40e852aef61b
Sivabala V <23d247@psgtech.ac.in>
2026-07-27 17:52:52 +0530

    [912] [Time Beats: 74.33%] [Memory Beats: 30.52%] - LeetPush

--- Sivabala06/leetcode-files/912-Sort-an-Array.py
+++ Sivabala06/leetcode-files/912-Sort-an-Array.py
@@ -1,38 +1,16 @@
+import random
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-
-        def merges(nums):
-            if len(nums)>1:
-                mid=(len(nums)//2)
-                left = nums[:mid]
-                right=nums[mid:]
-
-                merges(left)
-                merges(right)
-
-                lp=0
-                rp=0
-                fp=0
-                
-                while lp<len(left) and rp<len(right):
-                    if left[lp]<right[rp]:
-                        nums[fp]=left[lp]
-                        lp+=1
-                    else:
-                        nums[fp]=right[rp]
-                        rp+=1
-                    fp+=1
-                while lp<len(left):
-                    nums[fp]=left[lp]
-                    lp+=1
-                    fp+=1
-                while rp<len(right):
-                    nums[fp]=right[rp]
-                    rp+=1
-                    fp+=1
-            return nums
-        k=merges(nums)
-        return nums
+        
+        def quick(nums):
+            if len(nums)<=1:
+                return nums
+            pivot=random.choice(nums)
+            left=[i for i in nums if i<pivot]
+            right=[i for i in nums if i>pivot]
+            midd=[i for i in nums if i==pivot]
+            return quick(left)+midd+quick(right)        
+        return quick(nums)
 
 
 
