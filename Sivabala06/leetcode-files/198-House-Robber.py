aa1c2e4c0a1f09f8733a834e083728146b736d62
Sivabala V <23d247@psgtech.ac.in>
2026-08-11 12:42:03 +0530

    [198] [Time Beats: 100.00%] [Memory Beats: 23.51%] - LeetPush

---
+++ Sivabala06/leetcode-files/198-House-Robber.py
@@ -0,0 +1,20 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n=len(nums)
+        # lsum=0
+        # rsum=0
+        # for i in range(0,n):
+        #     if i%2==0 or i==0:
+        #         lsum+=nums[i]
+        #     elif i%2 !=0:
+        #         rsum+=nums[i]
+            
+        # return max(lsum,rsum,n)
+        dp=[0]*(n+1)
+        dp[0]=0
+        dp[1]=nums[0]
+        for i in range(1,n):
+            dp[i+1]=max(dp[i],dp[i-1]+nums[i])
+        return dp[n]
+
+        
