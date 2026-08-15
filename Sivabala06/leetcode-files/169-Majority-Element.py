bf6d8f824382f63cee3388f1179c4be98a761bd7
Sivabala V <23d247@psgtech.ac.in>
2026-08-15 12:33:13 +0530

    [169] [Time Beats: 22.90%] [Memory Beats: 84.04%] - LeetPush

--- Sivabala06/leetcode-files/169-Majority-Element.py
+++ Sivabala06/leetcode-files/169-Majority-Element.py
@@ -1,12 +1,21 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        count =0
-        candidate=None
-        for num in nums:
-            if count==0:
-                candidate=num
-            if num==candidate:
-                count+=1
+        # count =0
+        # candidate=None
+        # for num in nums:
+        #     print(candidate)
+        #     if count==0:
+        #         candidate=num
+        #     if num==candidate:
+        #         count+=1
+        #     else:
+        #         count-=1
+        # return candidate
+        counter={}
+        for i in nums:
+            if i in counter:
+                counter[i]+=1
             else:
-                count-=1
-        return candidate
+                counter[i]=1
+        k=max(counter,key=counter.get)
+        return k
