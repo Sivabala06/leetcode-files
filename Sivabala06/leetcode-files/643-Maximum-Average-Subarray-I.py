3008f44108fc3bb097b2e552968877a38cd2aab8
Sivabala V <23d247@psgtech.ac.in>
2026-08-16 18:26:59 +0530

    [643] [Time Beats: 11.82%] [Memory Beats: 16.56%] - LeetPush

--- Sivabala06/leetcode-files/643-Maximum-Average-Subarray-I.py
+++ Sivabala06/leetcode-files/643-Maximum-Average-Subarray-I.py
@@ -2,17 +2,17 @@ class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
         left=0
         right=0
-        maxs=[]
+        maxs=float(-inf)
         sums=0
         while(right<len(nums)):
             sums+=nums[right]
             
             if (right-left+1)==k:
-                maxs.append(sums/k)
+                maxs=max(maxs,sums)
                 sums-=nums[left]
                 left+=1
             right+=1
-        return max(maxs)
+        return maxs/k
                 
             
 
