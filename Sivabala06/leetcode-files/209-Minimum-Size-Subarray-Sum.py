25c8f55aacf9d373c368dcf23fe0fe0b1395349c
Sivabala V <23d247@psgtech.ac.in>
2026-08-16 18:03:35 +0530

    [209] [Time Beats: 19.66%] [Memory Beats: 41.65%] - LeetPush

--- Sivabala06/leetcode-files/209-Minimum-Size-Subarray-Sum.py
+++ Sivabala06/leetcode-files/209-Minimum-Size-Subarray-Sum.py
@@ -2,14 +2,14 @@ class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
         left=0
         right=0
-        mins =[]
+        mins =100000000000
         subsum=0
         while(right<len(nums)):
             subsum+=nums[right]
             while subsum >= target:
-                mins.append(right-left+1)
+                mins=min(mins,right-left+1)
                 subsum-=nums[left]
                 left+=1
             right+=1
 
-        return 0 if len(mins)==0 else min(mins)
+        return 0 if mins==100000000000 else mins
