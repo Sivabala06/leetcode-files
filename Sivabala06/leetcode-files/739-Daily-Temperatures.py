c89489974ba437b5f5772e169c408e7a77dc0671
Sivabala V <23d247@psgtech.ac.in>
2026-09-15 20:32:54 +0530

    [739] [Time Beats: 66.52%] [Memory Beats: 69.72%] - LeetPush

--- Sivabala06/leetcode-files/739-Daily-Temperatures.py
+++ Sivabala06/leetcode-files/739-Daily-Temperatures.py
@@ -2,25 +2,16 @@ class Solution:
     def dailyTemperatures(self, nums: List[int]) -> List[int]:
         n=len(nums)
         dp=[0]*n
+        stack = []
         for i in range(n):
-           if i== n-1:
-            dp[i]=0
-            break
-           if nums[i+1]>nums[i]:
-            dp[i]=1
-           if nums[i]>nums[i+1]:
-            dp[i]=1
-            for j in range(i+1,n):
-                if j==n-1 and nums[i]>nums[j]:
-                    dp[i]=0
-                    break
-                if nums[i]>nums[j]:
-                    dp[i]+=1
-                    continue
-                
-                elif nums[i]<nums[j]:
-                    break
+            while stack and nums[i] > nums[stack[-1]]:
+                j = stack.pop()
+                dp[j] = i - j
+
+            stack.append(i)
         return dp
+
+                
                 
            
         
