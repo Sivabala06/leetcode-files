81324884eaf852e5bf9d1c29784521bd8be0477a
Sivabala V <23d247@psgtech.ac.in>
2026-09-16 23:01:50 +0530

    [496] [Time Beats: 100.00%] [Memory Beats: 30.21%] - LeetPush

--- Sivabala06/leetcode-files/496-Next-Greater-Element-I.py
+++ Sivabala06/leetcode-files/496-Next-Greater-Element-I.py
@@ -4,16 +4,28 @@ class Solution:
         m=len(nums2)
         dp=[-1]*n
         
-        for i in range(n):
-            j=nums2.index(nums1[i])
-            for k in range(j+1,m):
-                if nums2[k]>nums2[j]:
-                    dp[i]=nums2[k]
-                    break
-            
+        # for i in range(n):
+        #     j=nums2.index(nums1[i])
+        #     for k in range(j+1,m):
+        #         if nums2[k]>nums2[j]:
+        #             dp[i]=nums2[k]
+        #             break
 
+        # return dp
+
+        stack=[]
+        b = {}
+
+        for j in range(len(nums2)):
+            while stack and nums2[j] > nums2[stack[-1]]:
+                k = stack.pop()
+                b[nums2[k]] = nums2[j]
+
+            stack.append(j)
+
+        for i in range(len(nums1)):
+            dp[i] = b.get(nums1[i], -1)
         return dp
-
 
 
 
