6d502835d603ce14de9c24c3a558efd1b0a45a07
Sivabala V <23d247@psgtech.ac.in>
2026-09-18 19:02:03 +0530

    [49] [Time Beats: 41.15%] [Memory Beats: 46.02%] - LeetPush

--- Sivabala06/leetcode-files/49-Group-Anagrams.py
+++ Sivabala06/leetcode-files/49-Group-Anagrams.py
@@ -1,34 +1,31 @@
 class Solution:
     def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
-        cop=[]
-        ans=[]
-        
-        
-        for i in strs:
-            j="".join(sorted(i))
-            cop.append(j)
-        kop=list(set(cop))
-         
+        # cop=[]
+        # ans=[]
+        # for i in strs:
+        #     j="".join(sorted(i))
+        #     cop.append(j)
+        # kop=list(set(cop))    
+        # for i in range(len(kop)):
+        #     res=[]
+        #     c=0
+        #     for j in range(len(cop)):
+        #         if kop[i]==cop[j]:
+        #             res.append(strs[j])  
+        #     ans.append(res) 
+        # return  ans   
             
-        for i in range(len(kop)):
-            res=[]
-            c=0
-            for j in range(len(cop)):
-                if kop[i]==cop[j]:
-                    res.append(strs[j])
-                 
-            ans.append(res)
-            
-                    
-            
-       
-            
-        
+        grp={}
 
+        for w in strs:
+            key="".join(sorted(w))
 
-        return  ans   
+            if key not in grp:
+                grp[key]=[]
+            grp[key].append(w)
+
+        return list(grp.values())
             
-
 
 
 
