580f5edcd9314eae17110789c6479b003f0754a5
Sivabala V <23d247@psgtech.ac.in>
2026-09-22 11:39:25 +0530

    [424] [Time Beats: 29.82%] [Memory Beats: 16.60%] - LeetPush

--- Sivabala06/leetcode-files/424-Longest-Repeating-Character-Replacement.py
+++ Sivabala06/leetcode-files/424-Longest-Repeating-Character-Replacement.py
@@ -1,3 +1,29 @@
+# class Solution:
+#     def characterReplacement(self, s: str, k: int) -> int:
+#         freq = {}
+#         left = 0
+#         answer = 0
+
+#         for right in range(len(s)):
+
+#             if s[right] not in freq:
+#                 freq[s[right]] = 0
+
+#             freq[s[right]] += 1
+
+#             ws = right - left + 1
+#             mf = max(freq.values())
+
+#             while ws - mf > k:
+#                 freq[s[left]] -= 1
+#                 left += 1
+
+#                 ws = right - left + 1
+#                 mf = max(freq.values())
+
+#             answer = max(answer, ws)
+
+#         return answer
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         freq = {}
@@ -5,22 +31,12 @@ class Solution:
         answer = 0
 
         for right in range(len(s)):
+            freq[s[right]] = freq.get(s[right], 0) + 1
 
-            if s[right] not in freq:
-                freq[s[right]] = 0
-
-            freq[s[right]] += 1
-
-            ws = right - left + 1
-            mf = max(freq.values())
-
-            while ws - mf > k:
+            while right - left + 1 - max(freq.values()) > k:
                 freq[s[left]] -= 1
                 left += 1
 
-                ws = right - left + 1
-                mf = max(freq.values())
-
-            answer = max(answer, ws)
+            answer = max(answer, right - left + 1)
 
         return answer
