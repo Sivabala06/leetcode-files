0ff71efcb3dd16b74f8312c566ccd400259c9280
Sivabala V <23d247@psgtech.ac.in>
2026-09-24 21:58:37 +0530

    [242] [Time Beats: 46.80%] [Memory Beats: 94.86%] - LeetPush

--- Sivabala06/leetcode-files/242-Valid-Anagram.py
+++ Sivabala06/leetcode-files/242-Valid-Anagram.py
@@ -1,15 +1,15 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         g={}
-
+        f={}
         for i in s:
             if i not in g:
                 g[i]=0
             g[i]+=1
         for i in t:
-            if i in g:
-                g[i]-=1
-        
-        if all(value == 0 for value in g.values()):
+            if i not in f:
+                f[i]=0
+            f[i]+=1
+        if f==g:
             return True
         return False
